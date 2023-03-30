3d1a4ce4406f3e88c604ea69ff3800f2cac79a5b
2-GHurley <2-GHurley@BH-R20-32.local.gfmat.org>
2023-03-30 14:22:43 +0100

    30/03/2023, this program is to connect SQL to netbeans

---
+++ src/main/java/com/mycompany/examplesqlcconnector/DAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.examplesqlcconnector;
+
+/**
+ *
+ * @author 2-GHurley
+ */
+public class DAO {
+    public static void createTable (){
+        System.out.println("Hello world!");
+    }
+}
